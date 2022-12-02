9f7ef26105af232872a17d0c295edf45bd715d98
reynmor <reynmor@ya.ru>
2022-12-02 23:58:29 +0300

    fix: исправить ошибки после ревью

--- scripts/index.js
+++ scripts/index.js
@@ -1,41 +1,29 @@
 /* PopUp: show/close */
 
 let popUp = document.querySelector('.popup');
-let editButton = document.querySelector('.profile__edit-button');
 let closeButton = document.querySelector('.popup__close-button');
+let form = document.querySelector('.popup__form');
+let fieldName = document.querySelector('.popup__field_type_name');
+let fieldInfo = document.querySelector('.popup__field_type_info');
+
+let profileTitle = document.querySelector('.profile__title');
+let profileSubTitle = document.querySelector('.profile__subtitle');
+let editButton = document.querySelector('.profile__edit-button');
 
 function showPopUp() {
-  if (popUp.classList.contains('popup_opened') === false) {
-    popUp.classList.add('popup_opened');
-  }
+  popUp.classList.add('popup_opened');
+  fieldName.value = profileTitle.textContent;
+  fieldInfo.value = profileSubTitle.textContent;
 }
 
 function closePopUp() {
-  if (popUp.classList.contains('popup_opened') !== false) {
     popUp.classList.remove('popup_opened');
-  }
-  fieldName.value = profileTitle.textContent;
-  fieldInfo.value = profileSubTitle.textContent;
 }
 
 editButton.addEventListener('click', showPopUp);
 closeButton.addEventListener('click', closePopUp);
 
-/* Form's fields: value from profile__info */
-
-let profileTitle = document.querySelector('.profile__title');
-let profileSubTitle = document.querySelector('.profile__subtitle');
-let fieldName = document.querySelector('.popup__field_type_name');
-let fieldInfo = document.querySelector('.popup__field_type_info');
-
-fieldName.value = profileTitle.textContent;
-fieldInfo.value = profileSubTitle.textContent;
-
 /* Function submit */
-
-//let submitButton = document.querySelector('.popup__submit-button');
-
-let form = document.querySelector('.popup__container');
 
 function formSubmit(evt) {
   evt.preventDefault();
