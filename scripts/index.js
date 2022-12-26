86a6266a7aef823ef120ef06188911dea0252060
reynmor <reynmor@ya.ru>
2022-12-26 20:45:22 +0300

    fix: исправить ошибки после ревью №1

--- scripts/index.js
+++ scripts/index.js
@@ -1,11 +1,10 @@
 /* Variables */
 
 // Popups variables
-const popUps = document.querySelectorAll('.popup');
-const popUpProfile = document.querySelector('.popup_button_edit-profile');
-const popUpAdd = document.querySelector('.popup_button_add-element');
-const popUpPic = document.querySelector('.popup_element_pic');
-const closeButtons = document.querySelectorAll('.popup__close-button');
+const popupProfile = document.querySelector('.popup_button_edit-profile');
+const popupAdd = document.querySelector('.popup_button_add-element');
+const popupPic = document.querySelector('.popup_element_pic');
+const popupCloseButtons = document.querySelectorAll('.popup__close-button');
 const formProfile = document.querySelector('.popup__form_type_profile');
 const formAdd = document.querySelector('.popup__form_type_add');
 const fields = document.querySelectorAll('.popup__field');
@@ -19,43 +18,13 @@ const picTitle = document.querySelector('.popup__pic-title');
 //Profile variables
 const profileTitle = document.querySelector('.profile__title');
 const profileSubTitle = document.querySelector('.profile__subtitle');
-const editButton = document.querySelector('.profile__edit-button');
-const addButton = document.querySelector('.profile__add-button');
+const profileEditButton = document.querySelector('.profile__edit-button');
+const profileAddButton = document.querySelector('.profile__add-button');
 
 //Elements variables
 const elements = document.querySelector('.elements__items');
 const elementTemplateItem = document.querySelector('#elements__template-item').content;
-let elementItems = [
-  {
-    title: 'Шоанинский храм',
-    link: './images/Trip-pic-1.jpg'
-  },
-
-  {
-    title: 'Карачаевск',
-    link: './images/Trip-pic-2.jpg'
-  },
-
-  {
-    title: 'Гора Эльбрус',
-    link: './images/Trip-pic-3.jpg'
-  },
-
-  {
-    title: 'Пятигорск',
-    link: './images/Trip-pic-4.jpg'
-  },
-
-  {
-    title: 'Бештау',
-    link: './images/Trip-pic-5.jpg'
-  },
-
-  {
-    title: 'Малый Тау',
-    link: './images/Trip-pic-6.jpg'
-  }
-];
+import {elementItems} from './initialElements.js';
 
 
 /* Functions */
@@ -65,33 +34,30 @@ let elementItems = [
 const createElement = function({title, link})
 {
   const element = elementTemplateItem.cloneNode(true);
-  const trashButton = element.querySelector('.elements__trash-button');
-  const likeButton = element.querySelector('.elements__like-button');
-  const pic = element.querySelector('.elements__pic');
+  const elementTrashButton = element.querySelector('.elements__trash-button');
+  const elementLikeButton = element.querySelector('.elements__like-button');
+  const elementPic = element.querySelector('.elements__pic');
 
   //Get info from object
   element.querySelector('.elements__title').textContent = title;
-  element.querySelector('.elements__pic').src = link;
-  element.querySelector('.elements__pic').alt = title;
+  elementPic.src = link;
+  elementPic.alt = title;
 
   //Delete element by button
-  trashButton.addEventListener('click', () => {
-    const item = trashButton.closest('.elements__item');
+  elementTrashButton.addEventListener('click', () => {
+    const item = elementTrashButton.closest('.elements__item');
     item.remove();
-
-    elementItems.pop(element);
-    console.log(elementItems)
   });
 
   //Like/dislike button
-  likeButton.addEventListener('click', () => {
-    likeButton.classList.toggle('elements__like-button_active')
+  elementLikeButton.addEventListener('click', () => {
+    elementLikeButton.classList.toggle('elements__like-button_active')
   });
 
   //Open large pic
-  pic.addEventListener('click', () => {
-    showPopUp(popUpPic);
-    showPic(pic)
+  elementPic.addEventListener('click', () => {
+    showPopup(popupPic);
+    showPic(elementPic)
   });
 
   return element;
@@ -100,7 +66,7 @@ const createElement = function({title, link})
 
 //Render some element
 const renderElement = function(element) {
-elements.prepend(element)
+  elements.prepend(element)
 };
 
 //Render initial elements
@@ -110,21 +76,20 @@ elementItems.reverse().map(item => {
 
 
 //Show some popup
-const showPopUp = function(element) {
+const showPopup = function(element) {
   element.classList.add('popup_opened');
 }
 
 //Close some popup
-const closePopUp = function(element) {
+const closePopup = function(element) {
   element.classList.remove('popup_opened');
 };
 
 //Close popups by button
-closeButtons.forEach(item => {
+popupCloseButtons.forEach(item => {
 
   item.addEventListener('click', function() {
-    popUp = item.closest('.popup');
-    closePopUp(popUp)
+    closePopup(item.closest('.popup'))
   })
 });
 
@@ -137,8 +102,8 @@ const cleanFields = function() {
 
 
 //Show edit-profile popup
-editButton.addEventListener('click', function() {
-  showPopUp(popUpProfile);
+profileEditButton.addEventListener('click', function() {
+  showPopup(popupProfile);
 
   fieldName.value = profileTitle.textContent;
   fieldInfo.value = profileSubTitle.textContent
@@ -150,27 +115,26 @@ formProfile.addEventListener('submit', function(evt) {
 
   profileTitle.textContent = fieldName.value;
   profileSubTitle.textContent = fieldInfo.value;
-  closePopUp(popUpProfile);
+  closePopup(popupProfile);
 });
 
 
 //Show add-element popup
-addButton.addEventListener('click', function () {
-  showPopUp(popUpAdd)
+profileAddButton.addEventListener('click', function () {
+  showPopup(popupAdd)
 });
 
 //Add new element
 formAdd.addEventListener('submit', function(evt) {
   evt.preventDefault();
 
-  item = {
+  const item = {
     title: fieldTitle.value,
     link: fieldLink.value
   };
 
   renderElement(createElement(item));
-  elementItems.push(item);
-  closePopUp(popUpAdd);
+  closePopup(popupAdd);
 
   cleanFields()
 });
@@ -178,38 +142,8 @@ formAdd.addEventListener('submit', function(evt) {
 
 //Render pic-popup
 const showPic = function(pic) {
-  elementTitle = pic.closest('.elements__item').querySelector('.elements__title');
+  const elementTitle = pic.closest('.elements__item').querySelector('.elements__title');
   picLarge.src = pic.src;
+  picLarge.alt = elementTitle.textContent;
   picTitle.textContent = elementTitle.textContent;
 }
-
-/*
-elements.addEventListener('click', element => {
-
-  trashButtons = document.querySelectorAll('.elements__trash-button');
-  trashButtons.forEach(item => {
-    if (item == element.composedPath()[0]) {
-      const li = element.composedPath()[0].closest('.elements__item');
-      li.remove();
-
-      elementItems.pop(element);
-      console.log(elementItems)
-    }
-  });
-
-  likeButtons = document.querySelectorAll('.elements__like-button');
-  likeButtons.forEach(item => {
-    if (item == element.composedPath()[0]) {
-      item.classList.toggle('elements__like-button_active')
-    }
-  })
-
-  pics = document.querySelectorAll('.elements__pic');
-  pics.forEach(item => {
-    if (item == element.composedPath()[0]) {
-      showPopUp(popUpPic);
-      showPic(item)
-    }
-  })
-});
-*/
---
+++ scripts/initialElements.js
@@ -0,0 +1,31 @@
+export const elementItems = [
+  {
+    title: 'Шоанинский храм',
+    link: './images/Trip-pic-1.jpg'
+  },
+
+  {
+    title: 'Карачаевск',
+    link: './images/Trip-pic-2.jpg'
+  },
+
+  {
+    title: 'Гора Эльбрус',
+    link: './images/Trip-pic-3.jpg'
+  },
+
+  {
+    title: 'Пятигорск',
+    link: './images/Trip-pic-4.jpg'
+  },
+
+  {
+    title: 'Бештау',
+    link: './images/Trip-pic-5.jpg'
+  },
+
+  {
+    title: 'Малый Тау',
+    link: './images/Trip-pic-6.jpg'
+  }
+];
